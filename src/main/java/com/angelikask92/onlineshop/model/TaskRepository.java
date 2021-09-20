01828bc1e991168805fd338108a9a37f4c3f3775
Angelikaa0147 <angelika.solak@wp.pl>
2021-09-20 17:18:18 +0200

    inital commit

--- src/main/java/com/angelikask92/onlineshop/model/TaskRepository.java
+++ src/main/java/com/angelikask92/onlineshop/model/TaskRepository.java
@@ -1,6 +1,10 @@
 package com.angelikask92.onlineshop.model;
 
-public class TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface TaskRepository extends JpaRepository<Task, Integer> {
 
 
 }
